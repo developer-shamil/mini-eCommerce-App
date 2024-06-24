f57929116fcb8a4ff1a73d1e0ec16d1cb16cf405
Shamil kp <shamilkp7895@gmail.com>
2024-06-24 15:36:22 +0530

    products added in console using api

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,10 +1,8 @@
 alert("This Is Shamil's Assignment work For Upcode Bootcamp");
 
-async function getAllProducts() {
-    var productResponse = await fetch('https://fakestoreapi.com/products',{
-        method:'GET'
-    });
-    var data = await productResponse.json();
-    console.log(data);
+async function getProducts() {
+    const response = await fetch('https://fakestoreapi.com/products');
+    const products = await response.json();
+    console.log(products)
 }
-getAllProducts();
+getProducts();
